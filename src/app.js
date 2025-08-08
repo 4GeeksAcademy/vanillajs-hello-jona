c477e8135b3ae4b3261fd320412d7a5db9f10871
Jsoriano <jsorianoh89@gmail.com>
2025-08-08 16:19:47 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+  let randonWho = who[Math.floor(Math.random() * who.length)]
+  let randonAction = action[Math.floor(Math.random() * action.length)]
+  let randonWhat = what[Math.floor(Math.random() * what.length)]
+  let randonWhen = when[Math.floor(Math.random() * when.length)]
+
+
+  let randomSentence = `${randonWho} ${randonAction} ${randonWhat} ${randonWhen}`
+
+  document.getElementById("excuse").innerHTML = randomSentence;
+  console.log(randomSentence);
+
+
 };
