9e1667154b32d71464ce03ea2d0f5b01e84e349c
Jsoriano <jsorianoh89@gmail.com>
2025-08-09 21:39:32 +0000

    Nuevo commit

--- src/app.js
+++ src/app.js
@@ -2,24 +2,19 @@ import "bootstrap";
 import "./style.css";
 
 
-window.onload = function() {
-  //write your code here
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+  function generateExcuse() {
+      const randomElement = arr => arr[Math.floor(Math.random() * arr.length)];
+      return `${randomElement(who)} ${randomElement(action)} ${randomElement(what)} ${randomElement(when)}.`;
+    }
 
-  let randonWho = who[Math.floor(Math.random() * who.length)]
-  let randonAction = action[Math.floor(Math.random() * action.length)]
-  let randonWhat = what[Math.floor(Math.random() * what.length)]
-  let randonWhen = when[Math.floor(Math.random() * when.length)]
-
-
-  let randomSentence = `${randonWho} ${randonAction} ${randonWhat} ${randonWhen}`
-
-  document.getElementById("excuse").innerHTML = randomSentence;
-  console.log(randomSentence);
+    document.getElementById('excuse').innerText = generateExcuse();
 
 
-};
+ 
+
+
